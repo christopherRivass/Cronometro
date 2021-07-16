bdd7d4ef67a3235cd8ed5e85be3f8bbdd1c1024e
christopherRivass <christopherrivas422@gmail.com>
2021-07-15 18:37:35 -0600

    i add 'class' in index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 const btn_start=document.querySelector('.btn-start');
 const btn_stop=document.querySelector('.btn-stop');
 const btn_reset=document.querySelector('.btn-reset');
+
+class cronometro{
+    constructor() {
+        this.milisecond=0;
+        this.second=0;
+        this.minut=0;
+        this.hours=0;
+    }
+    start(bolean){
+        if(bolean == true){
+            this.milisecond++;
+            if(this.milisecond == 60)return this.second++,this.milisecond=0;
+            if(this.second == 60)return  this.minut++,this.second=0;
+            if(this.minut == 60)return  this.hours++,this.minut=0;
+
+        }
+    }
+}
+
