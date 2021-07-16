910c861e6056d2c3124a4ae11bc649290f221efb
christopherRivass <christopherrivas422@gmail.com>
2021-07-15 21:17:45 -0600

    i do second, minut and hours of timer

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
+//btn for cronoment
 const btn_start=document.querySelector('.btn-start');
 const btn_stop=document.querySelector('.btn-stop');
 const btn_reset=document.querySelector('.btn-reset');
+//
+
+const primSecon=document.querySelector('.seconds');
+const primMinut= document.querySelector('.minut');
+const primHours = document.querySelector('.hours')
 
 class cronometro{
     constructor() {
@@ -12,11 +18,27 @@ class cronometro{
     start(bolean){
         if(bolean == true){
             this.milisecond++;
-            if(this.milisecond == 60)return this.second++,this.milisecond=0;
+            if(this.milisecond == 10000000)return this.second++,this.milisecond=0;
             if(this.second == 60)return  this.minut++,this.second=0;
             if(this.minut == 60)return  this.hours++,this.minut=0;
-
         }
+    }
+    shoe(){
+
     }
 }
 
+clock=new cronometro();
+
+primHours.innerHTML = clock.hours;
+
+
+btn_start.addEventListener('click', (e)=>{
+    while (e.isTrusted){
+        primSecon.innerHTML = clock.second;
+        primMinut.innerHTML = clock.minut;
+        clock.start(true);
+        if(clock.minut==1)return e.stopPropagation();
+    }
+
+});
